48698fbb2e1f897a003aaab11a632c190c9d475c
Levon Ghasabyan <levon.ghasabyan@lab655m.tumo.lab>
2017-11-01 15:00:22 +0400

    fix versions

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from oauth2client.file import Storage
 
 from utils import logger, extract_all_files, show_courses, show_course_works, p
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 try:
     import argparse
